69c806060ea9be47200d72e078edf9ec10d71ead
Jackson Pereira <jacksonga@hotmail.com>
2018-09-21 01:55:33 -0300

    Finalização do menu

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,4 @@
-app.controller('Controller', function ($scope) {
+app.controller('CtrlPrincipal', function($scope) {
     $scope.contatos = [
         {
             nome: "Jackson",
@@ -109,5 +109,5 @@ app.controller('Controller', function ($scope) {
             comercial: "(11) 4444-3333",
             observacoes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         }
-    ]
+    ];
 });
